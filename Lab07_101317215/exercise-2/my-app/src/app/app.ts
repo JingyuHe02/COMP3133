2764014c304d43c7fd94e143e533c548e72332ce
Jingyu He <101317215@georgebrown.ca>
2026-03-27 19:11:16 -0400

    lab08 finish

--- Lab07_101317215/exercise-2/my-app/src/app/app.ts
+++ Lab07_101317215/exercise-2/my-app/src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RemoveSpacesPipe } from './remove-spaces.pipe';
+import { InputFormatDirective } from './input-format.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RemoveSpacesPipe, InputFormatDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = signal('Jingyu Angular Lab07');
+
+  title = signal('Jingyu Angular Lab08');
+
+  heroes = [
+    { id: 1, name: 'Spider-Man' },
+    { id: 2, name: 'Iron-Man' },
+    { id: 3, name: 'Captain-America' }
+  ];
+
+  selectedHero: any = null;
+
+  selectHero(hero: any) {
+    this.selectedHero = hero;
+  }
 }
---
+++ Lab07_101317215/exercise-2/my-app/src/app/remove-spaces.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'removeSpaces'
+})
+export class RemoveSpacesPipe implements PipeTransform {
+
+  transform(value: string): string {
+    if (!value) return '';
+    return value.replace(/\s+/g, '');
+  }
+
+}
